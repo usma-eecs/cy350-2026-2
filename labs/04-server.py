128ccc022dc7f7c8217c375a821d9edefd311834
Sonny Bhatia <aadityabhatia@gmail.com>
2026-01-13 12:32:25 -0500

    lab 4

---
+++ labs/04-server.py
@@ -0,0 +1,32 @@
+import socket
+
+
+def main() -> None:
+    host = "0.0.0.0"
+    port = 8000
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((host, port))
+        s.listen(5)
+        print(f"Listening on {host}:{port}")
+    finally:
+        s.close()
+        print(f"Listening on {host}:{port}")
+
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print(f"Client connected: {addr}")
+            while True:
+                data = conn.recv(4096)
+                if not data:
+                    break
+                text = data.decode("utf-8")
+                response = text.upper()
+                responseBytes = response.encode("utf-8")
+                conn.sendall(responseBytes)
+
+
+if __name__ == "__main__":
+    main()
